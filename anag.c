642d2b7da18db7e907a3f80bd783aa66c3312fd5
Toby Poynder <github@whatcouldpossibly.com>
2019-11-03 11:28:24 +0000

    Old (21 years) anagram program first version

---
+++ anag.c
@@ -0,0 +1,56 @@
+/* - anag.c - print out all the possible anagrams of a word */
+
+char id[] = "@(#) anag v1.0  Print permutations of a word Author Toby Poynder";
+
+main(argc,argv)
+int argc;
+char **argv;
+{
+     char array[10];
+     void perms();
+     char *swap();
+     char *gets();
+     void perm();
+     int n = 0;
+     int length;
+
+
+     length = strlen(argv[1]);
+
+     perms(argv[1],n,length);
+}
+char *swap(string,pos_1,pos_2)
+char string[10];
+int pos_1,pos_2;
+{
+     char temp;
+
+     temp = string[pos_1];
+     string[pos_1] = string[pos_2];
+     string[pos_2] = temp;
+
+     return(string);
+}
+void perms(string,stage,length)
+char string[10];
+int stage,length;
+{
+	char temp[10];
+	int i;
+
+	if (length - stage == 2) {
+		strcpy(temp,string);
+		printf("%s\n",temp);
+		swap(temp,length-1,length-2);
+		printf("%s\n",temp);
+	} else {
+		strcpy(temp,string);
+		for (i = stage; i < length; ++i) {
+			swap(temp,stage,i);
+			perms(temp,stage+1,length);
+		}
+	}
+	return;
+}
+
+
